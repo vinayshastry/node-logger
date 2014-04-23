6475c5841d9c2ee80ef3ef89a2100df757bb6a52
vinay <vinayd@thoughtworks.com>
2014-04-23 13:33:13 +0530

    adding dependencies and the basic req/res logger

---
+++ index.js
@@ -0,0 +1,37 @@
+module.exports.create=function(logger) {
+    return function(req, res, next){
+        var rEnd = res.end;
+        req._rlStartTime = (new Date);
+
+        var req_msg = {
+            method: req.method,
+            url: req.url,
+            queryParams: req['query']
+        }
+        logger.info('request: ', req_msg);
+
+        res.end = function(chunk, encoding) {
+            // Do the work expected
+            res.end = rEnd;
+            res.end(chunk, encoding);
+            if(req.body) {
+                req_msg.body = JSON.stringify(req.body);
+                logger.debug('request: ', req_msg);
+            }
+
+            var res_msg = {
+                method: req.method,
+                url: req.url,
+                status: res.statusCode,
+                time: ((new Date) - req._rlStartTime) + 'ms'
+            }
+            logger.info('response: ', res_msg);
+
+            if(chunk) {
+                res_msg.body = chunk;
+                logger.debug('response: ', res_msg);
+            }
+        }
+        next();
+    };
+}
