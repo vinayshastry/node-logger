b3ee5ee90743c31c44c854efeb9f42f7c031e3da
vinay <vinayd@thoughtworks.com>
2014-04-23 15:20:34 +0530

    making update-level to be a function taking logger

--- src/resource/update-level.js
+++ src/resource/update-level.js
@@ -1,18 +1,20 @@
-var logger = require("../lib/logger")
 
-/**
- * PUT /logger
- * {log_level: "debug"}
- * Cookie: auth-token=validToken
- */
-module.exports = function(req, res) {
-    var new_level = req.body.log_level;
-    if(new_level) {
-        Object.keys(logger.transports).forEach(function(transport) {
-            logger.transports[transport].level = new_level;
-        });
-        return res.send(200);
-    } else {
-        return res.send(422);
+module.exports = function(logger) {
+    /**
+     * PUT /logger
+     * {log_level: "debug"}
+     * Cookie: auth-token=validToken
+     */
+    return function(req, res) {
+        var new_level = req.body.log_level;
+        if(new_level) {
+            Object.keys(logger.transports).forEach(function(transport) {
+                logger.transports[transport].level = new_level;
+            });
+            return res.send(200);
+        } else {
+            return res.send(422);
+        }
+
     }
 }
